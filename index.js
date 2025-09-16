97970e903bf06982b695026182e5971941a9b633
Ndet0 <festusndet00@gmail.com>
2025-09-16 12:36:11 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+//console.log(superbowlWin([
+  //{ year: "2018", result: "N/A" },
+  //{ year: "2017", result: "N/A" },
+  //{ year: "2016", result: "N/A" },
+  //{ year: "2015", result: "W" },
+  //{ year: "2014", result: "N/A" }
+//])); // Output: "2015"
+
+//console.log(superbowlWin([
+  //{ year: "2018", result: "N/A" },
+  //{ year: "2017", result: "N/A" },
+  //{ year: "2016", result: "N/A" },
+  //{ year: "2015", result: "N/A" },
+  //{ year: "2014", result: "N/A" }
+//])); // Output: undefined   
